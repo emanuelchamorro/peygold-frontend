9991b01b0b2c4e4b2a8ae7742f37335d8f33fdcd
Mayly Gibbs <mayly.gibbs@insoftar.com>
2020-08-07 14:02:05 -0400

    coorecciones

--- src/app/modules/eu-peygold/components/eu-pey-qr-scanner/eu-pey-qr-scanner.component.ts
+++ src/app/modules/eu-peygold/components/eu-pey-qr-scanner/eu-pey-qr-scanner.component.ts
@@ -19,32 +19,42 @@ export class EuPeyQrScannerComponent extends BaseComponent implements OnInit {
   private camera = false;
 
 
-  @ViewChild(QrScannerComponent,{ static: false }) qrScannerComponent: QrScannerComponent;
+  @ViewChild(QrScannerComponent, { static: false }) qrScannerComponent: QrScannerComponent;
   @ViewChild('result', { static: false }) resultElement: ElementRef;
-  @ViewChild('step1',{static: false}) step1:ElementRef;
+  @ViewChild('step1', { static: false }) step1: ElementRef;
   private video: any;
   public value: any;
   public validQR = false;
   step: number = 1;
   public decode: any;
   private user: User;
-  private callReaderQR:boolean;
-  
+  private callReaderQR: boolean;
+
+  private title: string;
+  private message: string;
+  private showImageBottom: boolean;
+  private routeTo: string;
+  private buttonLabel: string;
+
+  //TODO: PARA LA NUEVA IMPLEMENTACION DE PAGO POR QR
+  payments: any;
+  private transaction: Transaction;
+
 
   constructor(private renderer: Renderer2,
     private authService: AuthService,
     private transactionsService: TransactionsService,
-    private spinnerService:NgxSpinnerService) {
+    private spinnerService: NgxSpinnerService) {
 
-      super();
-    }
+    super();
+  }
 
   ngOnInit() {
     this.user = this.authService.user();
 
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
 
     this.qrScannerComponent.getMediaDevices().then(devices => {
       const videoDevices: MediaDeviceInfo[] = [];
@@ -68,24 +78,43 @@ export class EuPeyQrScannerComponent extends BaseComponent implements OnInit {
         }
       }
     }).catch(
-      (error)=>{
+      (error) => {
         this.setError("Ha ocurrido un error. Camara no disponible.");
       }
     );
 
-    this.qrScannerComponent.capturedQr.subscribe(result => { 
+    this.qrScannerComponent.capturedQr.subscribe(result => {
       console.log(result);
-      
+
       this.deactivateCamera();
       if (this._isValidQR(JSON.parse(result))) {
         this.decode = JSON.parse(result);
         this.decode.fullName = atob(this.decode.fullName);
-        this.step++;
-      }else{
+        //TODO: Consultar cobros pendientes mas recientes
+        const haveCobros = false;
+        if (haveCobros) {
+          this.payments = [{ idTransactionType: 1, ammount: 1000 }];
+          this.step = 3;
+        } else {
+          this.step = 2;
+        }
+      } else {
         this.setError("El código QR no es válido.");
       }
 
     });
+  }
+
+  /**
+ * set transaction
+ * @param transaction 
+ */
+  setTransaction(transaction: Transaction) {
+    transaction.receiver = this.user;
+    this.transaction = transaction;
+    this.payments = JSON.parse(this.transaction.paymentsToQR);
+    console.log(transaction)
+    this.step++;
   }
 
   activateCamera() {
@@ -113,32 +142,32 @@ export class EuPeyQrScannerComponent extends BaseComponent implements OnInit {
     const reader = new FileReader();
     reader.readAsDataURL(files[0]);
     reader.onload = (event) => {
-      try{
+      try {
         this.callReaderQR = false;
         this.value = reader.result;
         this.validQR = true;
-        console.log('value en preview',this.value)
-       setTimeout(()=>{
-         if(!this.callReaderQR){
-          this.value=null; 
-          let element: Element = this.renderer.createElement('h5');  
-          element.innerHTML = "El código QR no es válido.";
-          element.className = "invalid-feedback text-center";
-          this.renderElement(element);
-         }
-                 
-        },500);
-      }catch(error){
-        console.log('error',error);
+        console.log('value en preview', this.value)
+        setTimeout(() => {
+          if (!this.callReaderQR) {
+            this.value = null;
+            let element: Element = this.renderer.createElement('h5');
+            element.innerHTML = "El código QR no es válido.";
+            element.className = "invalid-feedback text-center";
+            this.renderElement(element);
+          }
+
+        }, 500);
+      } catch (error) {
+        console.log('error', error);
       }
 
 
     };
   }
 
-  readQR(e) {    
+  readQR(e) {
     this.callReaderQR = true;
-    if (this._isValidQR(JSON.parse(e.result))) { 
+    if (this._isValidQR(JSON.parse(e.result))) {
       this.decode = JSON.parse(e.result);
       this.decode.fullName = atob(this.decode.fullName);
       const button1 = this.renderer.createElement('button');
@@ -148,17 +177,17 @@ export class EuPeyQrScannerComponent extends BaseComponent implements OnInit {
       const button2 = this.renderer.createElement('button');
       button2.className = "white-button-border mr-4";
       const buttonText2 = this.renderer.createText('Cancelar');
-      for (let node of this.resultElement.nativeElement.childNodes) {  
-        this.renderer.removeChild(this.resultElement.nativeElement, node);  
-      }  
+      for (let node of this.resultElement.nativeElement.childNodes) {
+        this.renderer.removeChild(this.resultElement.nativeElement, node);
+      }
       this.renderer.appendChild(button1, buttonText1);
       this.renderer.appendChild(button2, buttonText2);
-      this.renderer.appendChild(this.resultElement.nativeElement, button2); 
-      this.renderer.appendChild(this.resultElement.nativeElement, button1); 
+      this.renderer.appendChild(this.resultElement.nativeElement, button2);
+      this.renderer.appendChild(this.resultElement.nativeElement, button1);
       this.renderer.listen(button2, 'click', () => this.cancel());
-      this.renderer.listen(button1, 'click', () => this.nextStep());        
-    }else{
-      let element: Element = this.renderer.createElement('h5');  
+      this.renderer.listen(button1, 'click', () => this.nextStep());
+    } else {
+      let element: Element = this.renderer.createElement('h5');
       element.innerHTML = "El código QR no es válido.";
       element.className = "invalid-feedback text-center";
       this.renderElement(element);
@@ -167,31 +196,31 @@ export class EuPeyQrScannerComponent extends BaseComponent implements OnInit {
   }
 
   _isValidQR(dataDecode: any) {
-    console.log('dataDecode ',dataDecode)
-    if (dataDecode && dataDecode.fullName && dataDecode.email && dataDecode.payments && dataDecode.payments.length > 0) {
+    console.log('dataDecode ', dataDecode)
+    if (dataDecode && dataDecode.fullName && dataDecode.email) {
       return true;
     } else {
       return false;
     }
   }
 
-  renderElement(element) {  
-    for (let node of this.resultElement.nativeElement.childNodes) {  
-      this.renderer.removeChild(this.resultElement.nativeElement, node);  
-    }  
-    this.renderer.appendChild(this.resultElement.nativeElement, element);  
+  renderElement(element) {
+    for (let node of this.resultElement.nativeElement.childNodes) {
+      this.renderer.removeChild(this.resultElement.nativeElement, node);
+    }
+    this.renderer.appendChild(this.resultElement.nativeElement, element);
   }
 
-  cancel(){
+  cancel() {
     this.callReaderQR = false;
     window.location.reload();
   }
 
-  nextStep(){
+  nextStep() {
     this.callReaderQR = false;
-    let firstDiv:HTMLElement = this.step1.nativeElement;
+    let firstDiv: HTMLElement = this.step1.nativeElement;
     firstDiv.style.removeProperty("visibility");
-    firstDiv.style.setProperty('display','none');
+    firstDiv.style.setProperty('display', 'none');
     this.step++;
   }
 
@@ -250,14 +279,21 @@ export class EuPeyQrScannerComponent extends BaseComponent implements OnInit {
   }
 
   back() {
-    let firstDiv:HTMLElement = this.step1.nativeElement;
-    firstDiv.style.removeProperty("display");
-    firstDiv.style.setProperty('visibility','visible');
+    // let firstDiv:HTMLElement = this.step1.nativeElement;
+    // firstDiv.style.removeProperty("display");
+    // firstDiv.style.setProperty('visibility','visible');
     this.step--;
   }
 
   send() {
-    this.proccess(this.decode);
+    //this.proccess(this.decode);
+    this.title = "¡El pago fue enviado!";
+    this.message = `Has pagado exitosamente a <b>${this.decode.fullName}.</b> El importe total fue descontado de tu billetera Peygold,<br />verás la operacion reflejada en "Movimientos"`;
+    this.showImageBottom = false;
+    this.routeTo = this.routes.home.href;
+    this.buttonLabel = "Volver a inicio";
+
+    this.step++;
   }
 
 
