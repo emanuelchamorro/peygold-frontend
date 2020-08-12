c298b15b0c8fbce10bb1e685c4343705f7a6441a
Mayly Gibbs <mayly.gibbs@insoftar.com>
2020-08-12 16:51:52 -0400

    comercios adheridos 90%

--- src/app/modules/commons-peygold/components/ui-pey-single-map/ui-pey-single-map.component.ts
+++ src/app/modules/commons-peygold/components/ui-pey-single-map/ui-pey-single-map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter, Input, SimpleChanges } from '@angular/core';
 import { MapsAPILoader, GoogleMapsAPIWrapper } from "@agm/core";
+import { User } from '../../../../models/user';
+
 declare var google: any;
 
 @Component({
@@ -9,8 +11,8 @@ declare var google: any;
 })
 export class UiPeySingleMapComponent implements OnInit {
 
-  @Input('address') address: string;
-  @Output() onNewAddress: EventEmitter<string> = new EventEmitter<string>();
+  //@Input('address') address: string;
+  @Input('ecommerces') ecommerces: Array<any>;
   // initial center position for the map
   protected map: google.maps.Map;
   protected myMarker: any = null;
@@ -19,25 +21,12 @@ export class UiPeySingleMapComponent implements OnInit {
   public lng: number;
   protected geocoder: any = null;
   protected markerBounds;
+  protected markers: any[];
 
-
-  markers = [
-    {
-      lat: 10.4946141,
-      lng: -66.8154954,
-      title: "C.C Aloa, El marques."
-    },
-    {
-      lat: 10.4859436,
-      lng: -66.8210799,
-      title: "C.C Lides."
-    }
-  ]
-
-  constructor(private mapsAPILoader: MapsAPILoader, ) { }
+  constructor(private mapsAPILoader: MapsAPILoader) { }
 
   ngOnInit() {
-
+    this.markers = [];
     //Una Vez se pueda usar la API de google map se obtiene ubicación actual para obtener la dirección y se lanza el evento onNewAddress
     this.mapsAPILoader.load().then(() => {
       //Instancia objeto que calcula direcciones y geolocalizaciones
@@ -49,11 +38,15 @@ export class UiPeySingleMapComponent implements OnInit {
     });
   }
 
+  /** detente new chage in array ecommerces */
   ngOnChanges(changes: SimpleChanges) {
-    const { currentValue } = changes.address;
-    console.log(currentValue);
-    if (currentValue) {
-      this.addressToLatLng(currentValue);
+    console.log(changes);
+    let currentValue;
+    if (changes.ecommerces != undefined) {
+      currentValue = changes.ecommerces.currentValue;
+    }
+    if (currentValue && currentValue != '' && currentValue != undefined) {
+      this.loadAllMarkes();
     }
   }
 
@@ -62,6 +55,7 @@ export class UiPeySingleMapComponent implements OnInit {
     this.map = map;
     //this.map.addListener('click', this.placeMarkerAndPanTo);
 
+    console.log('ecommerces', this.ecommerces);
 
     this.loadAllMarkes();
     this.map.fitBounds(this.markerBounds);
@@ -78,16 +72,12 @@ export class UiPeySingleMapComponent implements OnInit {
 
   onSuccessGeolocating = (position) => {
     const { latitude, longitude } = position.coords;
-    console.log('latitude', latitude)
-    console.log('longitude', longitude)
+
     this.lat = latitude;
     this.lng = longitude;
-    let latLng: any;
-    latLng = new google.maps.LatLng(latitude, longitude);
-    //this.map.setCenter({ lat: latitude, lng: longitude });
-    this.myMarker = this.newMyMarker(latLng);
-    this.markerBounds.extend(latLng);
-    //this.latLngToAddress(latLng);
+
+    console.log('latitude', latitude)
+    console.log('longitude', longitude)
 
   }
 
@@ -98,27 +88,41 @@ export class UiPeySingleMapComponent implements OnInit {
     //this.onNewLatLng.emit([this.lat,this.lng]);
 
   }
+  /**
+   * add current location
+   */
+  addMyMarker = () => {
+    let latLng = new google.maps.LatLng(this.lat, this.lng);
 
-  newMyMarker = (latLng) => (
+    this.myMarker = new google.maps.Marker({
+      position: latLng,
+      map: this.map,
+      title: "Mi ubicacion",
+      icon: "/assets/images/ic_location_on_128_28437.png"
+    });
+    this.markers.push(this.myMarker);
+    this.markerBounds.extend(latLng);
+  };
+
+
+  /**
+   * add new mark to map
+   */
+  addNewMarker = (latLng) => {
     new google.maps.Marker({
       position: latLng,
       map: this.map,
-      title:"Mi ubicacion",
-      icon:"/assets/images/ic_location_on_128_28437.png"
+      title: "Tu busqueda",
+      icon: { url: '/assets/images/icon-map-02.svg', scaledSize: new google.maps.Size(40, 40) }
     })
-  );
+  };
 
-  newMarker = (latLng) => (
-    new google.maps.Marker({
-      position: latLng,
-      map: this.map,
-      title:"Tu busqueda",
-      icon:'/assets/images/bank_finance_dollar_location_pointer_icon_124684.png'
-    })
-  );
 
+  /**
+   * convert dir to lat and lng
+   * @param address 
+   */
   addressToLatLng(address: string) {
-
     this.geocoder.geocode({ 'address': address }, (results, status) => {
       if (status === 'OK') {
         console.log('address', address)
@@ -128,7 +132,7 @@ export class UiPeySingleMapComponent implements OnInit {
         console.log('lat', lat)
         console.log('lng', lng)
         this.deleteMarker();
-        this.newMarker(results[0].geometry.location);
+        this.addNewMarker(results[0].geometry.location);
         this.markerBounds.extend(results[0].geometry.location);
         this.map.fitBounds(this.markerBounds);
         //this.onNewLatLng.emit([lat,lng]);
@@ -140,24 +144,76 @@ export class UiPeySingleMapComponent implements OnInit {
 
   }
 
-  deleteMarker() {
-    if (this.marker != null)
-      this.marker.setMap(null);
-  }
+
+  /**
+   * Load and print markers into map
+   */
 
   loadAllMarkes() {
-    this.markers.forEach(itemMmark => {
-
-      const marker = new google.maps.Marker({
-        position: new google.maps.LatLng(itemMmark.lat, itemMmark.lng),
-        map: this.map,
-        title: itemMmark.title,
-        icon:'/assets/images/bank_finance_dollar_location_pointer_icon_124684.png'
+    this.deleteMarker();
+    this.addMyMarker();
+    this.ecommerces.forEach(ecommerce => {
+      let cardInfo = new google.maps.InfoWindow({
+        content: this.generateInfoCard(ecommerce),
       });
+      const marker = new google.maps.Marker({
+        position: new google.maps.LatLng(this.lat + this.getRandom(0.001, 0.003), this.lng + this.getRandom(0.001, 0.003)),
+        map: this.map,
+        title: ecommerce.completeName,
+        icon: { url: '/assets/images/icon-map-02.svg', scaledSize: new google.maps.Size(40, 40) }
+      });
+      marker.addListener('mouseover', () => {
+        cardInfo.open(this.map, marker);
+      })
+
+      marker.addListener('mouseout', () => {
+        cardInfo.close();
+      })
+
+      this.markers.push(marker);
       this.markerBounds.extend(marker.position);
     });
+  }
 
-    
+  /**
+   * Delete markers from  map
+   */
+  deleteMarker() {
+    if (this.markers && this.markers.length > 0) {
+      this.markers.forEach(mark => {
+        mark.setMap(null);
+      });
+    }
+    this.markers = [];
+  }
+
+  /**
+   * TODO: this it is temporary.
+   * @param min 
+   * @param max 
+   */
+
+  getRandom(min: number, max: number): number {
+    const num = (Math.random() * (max - min) + min).toFixed(3);
+    console.log('num', num);
+    return parseFloat(num);
+  }
+
+  generateInfoCard(ecommerce: User): string {
+
+    return `<div class="p-2">
+                <div class="content-avatar"><img class="img-card" src=" ${ecommerce.avatarURL}"/></div>
+
+                <div class="info-card">
+                    <h2>${ecommerce.completeName}</h2>
+                    <div>
+                        <i class="fas fa-map-marker-alt"></i><h6 class="address">Direccion, calle, ciudad, pais</h6>
+                    </div>
+                    <div><i class="fas fa-envelope"></i><h6 class="email">${ecommerce.email}</h6></div>
+                    <div><i class="fas fa-phone"></i><h6 class="phone">${ecommerce.phone}</h6></div>
+                </div>
+           </div>`
+
   }
 
 }
